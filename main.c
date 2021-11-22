067eb4d6e32b846030c25301638945cfad8dca33
JordanBreen <jm23bree@siena.edu>
2021-11-22 15:23:10 -0500

    Update

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <ncurses.h>
 //#include <ncursesw/ncurses.h>
 #include <sqlite3.h>
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
