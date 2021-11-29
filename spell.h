c5b032bb44c8ddcfd321cda4246155dd5adce4f1
JordanBreen <jm23bree@siena.edu>
2021-11-29 15:28:04 -0500

    Continued updates to members of spell struct

--- spell.h
+++ spell.h
@@ -11,36 +11,53 @@ typedef struct spell {
   char * school;               // enum
   char * subschool;            // enum
   char * descriptor;           // enum
-  char * spell_level;          // ptr array to 'classes'
+  char * spell_level_text;     // ptr array to 'classes'
   char * casting_time;         // struct
   char * components_text;      // text
   //char   costly_component;   // will be determined elsewhere, skip
-  char * range;                // struct
+  char * range;                // struct, enclosing enum CLOSE, MEDIUM, LONG
   char * area;                 // struct
   char * effect;               // struct
   char * targets;              // struct
   char * duration;             // struct
-  char   dismissible;          // bool 2
-  char   shapeable;            // bool 3
+  //char   dismissible;          // bool 2
+  //char   shapeable;            // bool 3
   char * saving_throw;         // struct
-  char * spell_resistance;     // bool 4
+  //char * spell_resistance;     // bool 4
   char * description_full;     // string
   char * description_short;    // string
   char * description_formated; // string
   char * source;               // enum
   char * full_text;            // string
+  // Components
   unsigned char components;    // bool flag (5), max 8 (unsigned char)
-  //char verbal;               // bool 1
-  //char somatic;              // bool 2
-  //char material;             // bool 3
-  //char focus;                // bool 4
-  //char divine_focus;         // bool 5
-  char * spell_level_by_class; // flag, range 0-9 (16) 4 bits ea, expecting max 16 classes, 4*16 = 64 bits == 8 bytes, unsigned long
+  // verbal;          // bool 01
+  // somatic;         // bool 02
+  // material;        // bool 03
+  // focus;           // bool 04
+  // divine_focus;    // bool 05
+  // Qualities
+  // spell_resistance // bool 06 (1)
+  // shapeable,       // bool 07 (2)
+  // dimissable       // bool 08 (3)
+  // mythic           // bool 09 (4)
+  // ruse             // bool 10 (5)
+  // draconic         // bool 11 (6)
+  // meditative       // bool 12 (7)
+  char * spell_level_by_class;
+  // flag,range0-9 4 bits ea, expecting max 16 classes, 4*16 = 64 bits == 8 bytes, unsigned long
+  // correction: expecting more classes, could possibly exclude additional classes (+10)
   char * SLA_level;            // possible part of another flag structure
   char * domain_text;          // string
+  char * bloodline_text;       // string
+  char * patron_text;          // string
+  char * mythic_text;          // string
+  char * mythic_augment_text;  // string
+  // char * haunt_stats_text;  // string possibly ignore
   char * descriptors;          // bool flag,  expecting 25 descriptors, 25 bits == >3 bytes, unsigned int, assert 4-byte int
-  char * link_text;            // string
+  // char * link_text;         // string, possibly skip, same as name member
   unsigned short id;           // max 65,535 spells, expecting ~1,600 spells
+  unsigned short *material_cost; // possible NULL value;
 
 
 
