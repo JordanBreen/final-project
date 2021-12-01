bf43165f1cf16ce16cbb44cf97b620912e2d7488
JordanBreen <jm23bree@siena.edu>
2021-12-01 01:37:43 -0500

    Removed references to icon.h (deleted)

--- sqlite_loader.h
+++ sqlite_loader.h
@@ -3,14 +3,14 @@
 
 #include <sqlite3.h>
 
-typedef enum datatype {
-  TYPE_ICON
-} datatype;
+typedef enum object {
+  SPELL
+} object;
 
 void  db_init        (sqlite3 **, const char *, sqlite3_stmt *, char *); 
-void  alloc_storage  (void **, datatype, int, int(**)(void *, int, char **, char **));
+void  alloc_storage  (void **, object, int, int(**)(void *, int, char **, char **));
 int   get_table_size (void *,  int, char **, char **);
-void *load_table     (const char *, const char *, datatype, int *);
+void *load_table     (const char *, const char *, object, int *);
 void  close_loader   (char *,  sqlite3 *);
 
 #endif
