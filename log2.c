9bac429b9378b2b374df5559c82bd7447f0dee41
JordanBreen <jm23bree@siena.edu>
2021-12-03 05:26:33 -0500

    replaced by logx

--- log2.c
+++
@@ -1,4 +0,0 @@
-//#include "log2.h"
-//double log2       (double arg) { return log(arg) / log(2);     }
-//int    log2_ceil  (double arg) { return (int)ceil(log2(arg);   }
-//int    log2_floor (double arg) { return (int)floor(log2(arg)); }
